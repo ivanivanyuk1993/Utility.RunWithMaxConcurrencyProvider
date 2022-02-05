7b38fd749cc981c38347a459bf2d3bb13050253b
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-02-05 15:15:37 +0300

    Added `RunWithMaxConcurrencyProvider`

---
+++ RunWithMaxConcurrencyProviderNS/RunWithMaxConcurrencyProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace RunWithMaxConcurrencyProviderNS;
+
+public static class RunWithMaxConcurrencyProvider
+{
+    public static void RunWithMaxConcurrency(Action action)
+    {
+        var finishEvent = new CountdownEvent(initialCount: Environment.ProcessorCount);
+        var startEvent = new ManualResetEvent(initialState: false);
+        var threadCreatedEvent = new CountdownEvent(initialCount: Environment.ProcessorCount);
+
+        for (var threadIndex = 0; threadIndex < Environment.ProcessorCount; threadIndex++)
+            new Thread(start: () =>
+            {
+                threadCreatedEvent.Signal();
+                startEvent.WaitOne();
+
+                action();
+
+                finishEvent.Signal();
+            })
+                .Start();
+
+        threadCreatedEvent.Wait();
+        startEvent.Set();
+        finishEvent.Wait();
+    }
+}
